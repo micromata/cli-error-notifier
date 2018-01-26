8e441b4e827ba66c78ca28ab10b07ee7f8b3e7b5
Michael Kühnel <m.kuehnel@micromata.de>
2018-01-26 17:00:41 +0100

    Add notification

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,7 @@ const cli = meow(`
 	  --title, -t    The title of the notification
 	  --message, -m  The message body of the notification
 	  --icon, -i     Can be any absolute path
-	  --sound, -s;   Use none to disable default sound notification
+	  --sound, -s;   Use "mute" to disable default sound notification
 
 	  Options are passed to the node-notifier CLI.
 	  See https://github.com/mikaelbr/node-notifier-cli#usage
@@ -23,7 +23,8 @@ const cli = meow(`
 
 	Examples
 	  $ onerror "wget unknown-host.xyz"
-	  $ onerror "wget unknown-host.xyz" -t Error -m "Something went wrong. Check the terminal."
+	  $ onerror "wget unknown-host.xyz" -s Mute
+	  $ onerror "wget unknown-host.xyz" -t Error -m "My error message"
 	  $ onerror "wget unknown-host.xyz"  -s Glass -i https://cdn.rawgit.com/npm/logos/31945b5c/npm%20square/n-64.png
 `, {
 	alias: {
@@ -59,6 +60,11 @@ if (cli.input.length !== 1) {
 
 if (typeof cli.input[0] !== 'string') {
 	console.log(`\n${logSymbols.error} Command needs to be a string, but got ${typeof cli.input[0]}. Please check the help below:`);
+	cli.showHelp(2);
+}
+
+if (Object.keys(cli.flags).map(key => typeof cli.flags[key]).some(type => type === 'boolean')) {
+	console.log(`\n${logSymbols.error} Wrong option(s) provided. Please check the help below:`);
 	cli.showHelp(2);
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const execa = require('execa');
+const notifier = require('node-notifier');
 
 module.exports = (command, flags) => {
 	flags = flags || {};
+
+	const notifierOpts = {
+		title: flags.t || flags.title || 'An error has occured',
+		message: flags.m || flags.message || 'Check the terminal for more information',
+		icon: flags.i || flags.icon || '',
+		sound: flags.s || flags.sound || true
+	};
+	notifierOpts.sound = /mute/i.test(notifierOpts.sound) ? false : notifierOpts.sound;
 
 	execa.shell(command, {env: {FORCE_COLOR: true}}).then(result => {
 		console.log(result.stdout);
 	}).catch(err => {
 		console.log(err.stdout);
+		notifier.notify(notifierOpts);
 		process.exit(err.code);
 	});
 };
