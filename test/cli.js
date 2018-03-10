7881c16bc99cf6f05e56dd0bd6841a748af290e7
Michael Kühnel <m.kuehnel@micromata.de>
2018-03-10 13:24:28 +0100

    test: add tests for showing help and version

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,8 @@
 import test from 'ava';
 import execa from 'execa';
+import isSemver from 'is-semver';
 
-test('Unknown command', async t => {
+test('"Unknown command"', async t => {
 	const result = await t.throws(execa('./cli.js', ['Hej']));
 	t.is(result.code, 127);
 	t.regex(result.stderr, /Hej:.+not found/);
@@ -22,23 +23,55 @@ test('"Command which exits properly with exit code 0"', async t => {
 	t.is(result.stderr, '');
 });
 
-test('No input given', async t => {
+test('"No input given"', async t => {
 	const result = await t.throws(execa('./cli.js', []));
 	t.is(result.code, 2);
 	t.regex(result.stdout, /Invalid input. Please check the help below:/);
 	t.regex(result.stdout, /Usage/);
 });
 
-test('To much input', async t => {
+test('"To much input"', async t => {
 	const result = await t.throws(execa('./cli.js', ['Hu', 'Ha']));
 	t.is(result.code, 2);
 	t.regex(result.stdout, /Invalid input. Please check the help below:/);
 	t.regex(result.stdout, /Usage/);
 });
 
-test('wrong options', async t => {
+test('"wrong options"', async t => {
 	const result = await t.throws(execa('./cli.js', ['echo Hej', '-p']));
 	t.is(result.code, 2);
 	t.regex(result.stdout, /Wrong option\(s\) provided. Please check the help below:/);
 	t.regex(result.stdout, /Usage/);
+});
+
+test('"Show version number via flag"', async t => {
+	await t.notThrows(execa('./cli.js', ['--version']));
+	const result = await execa('./cli.js', ['--version']);
+	t.true(isSemver(result.stdout));
+	t.is(result.code, 0);
+	t.is(result.stderr, '');
+});
+
+test('"Show version number via alias"', async t => {
+	await t.notThrows(execa('./cli.js', ['-v']));
+	const result = await execa('./cli.js', ['-v']);
+	t.true(isSemver(result.stdout));
+	t.is(result.code, 0);
+	t.is(result.stderr, '');
+});
+
+test('"Show help via flag"', async t => {
+	await t.notThrows(execa('./cli.js', ['--help']));
+	const result = await execa('./cli.js', ['--help']);
+	t.regex(result.stdout, /^\n {2}Sends native desktop notifications/m);
+	t.is(result.code, 0);
+	t.is(result.stderr, '');
+});
+
+test('"Show help via alias"', async t => {
+	await t.notThrows(execa('./cli.js', ['-h']));
+	const result = await execa('./cli.js', ['-h']);
+	t.regex(result.stdout, /^\n {2}Sends native desktop notifications/m);
+	t.is(result.code, 0);
+	t.is(result.stderr, '');
 });
