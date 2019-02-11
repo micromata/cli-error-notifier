d1527cdbcb5abb7859558af6748dd9c89a78b1f2
Michael Kühnel <m.kuehnel@micromata.de>
2019-02-11 16:48:41 +0100

    fix: Update dependencies

--- test/error-notifier.js
+++ test/error-notifier.js
@@ -2,21 +2,21 @@ import test from 'ava';
 import errorNotifier from '../lib/error-notifier';
 
 test('"Unknown command"', async t => {
-	const result = await t.throws(errorNotifier('Hej'));
+	const result = await t.throwsAsync(errorNotifier('Hej'));
 	t.is(result.stdout, '');
 	t.is(result.code, 127);
 	t.regex(result.stderr, /Hej:.+not found/);
 });
 
 test('"Command which fails with an exit code other than 0"', async t => {
-	const result = await t.throws(errorNotifier('tar'));
+	const result = await t.throwsAsync(errorNotifier('tar'));
 	t.is(result.stdout, '');
 	t.not(result.code, 0);
 	t.regex(result.stderr, /tar:.+must specify one of/i);
 });
 
 test('"Command which exits properly with exit code 0"', async t => {
-	await t.notThrows(errorNotifier('echo Hej'));
+	await t.notThrowsAsync(errorNotifier('echo Hej'));
 	const result = await errorNotifier('echo Hej');
 	t.is(result.stdout, 'Hej');
 	t.is(result.code, 0);
