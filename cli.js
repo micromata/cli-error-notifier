218c8f60294af3c5cb111b14ae8a647f6071f907
Michael Kühnel <m.kuehnel@micromata.de>
2022-09-12 10:29:03 +0200

    chore: Fix ESLint config and errors

--- cli.js
+++ cli.js
@@ -4,10 +4,11 @@ const meow = require('meow');
 const logSymbols = require('log-symbols');
 const updateNotifier = require('update-notifier');
 
-const pkg = require('./package.json');
+const package_ = require('./package.json');
 const errorNotifier = require('./lib/error-notifier');
 
-const cli = meow(`
+const cli = meow(
+	`
 	Usage
 	  $ onerror <command> [options]
 
@@ -30,32 +31,34 @@ const cli = meow(`
 	  $ onerror "wget unknown-host.xyz" -s mute
 	  $ onerror "wget unknown-host.xyz" -t Error -m "My error message"
 	  $ onerror "wget unknown-host.xyz"  -s Glass -i https://cdn.rawgit.com/npm/logos/31945b5c/npm%20square/n-64.png
-`, {
-	alias: {
-		h: 'help',
-		v: 'version'
+`,
+	{
+		alias: {
+			h: 'help',
+			v: 'version',
+		},
+		flags: {
+			title: {
+				type: 'string',
+				alias: 't',
+			},
+			message: {
+				type: 'string',
+				alias: 'm',
+			},
+			icon: {
+				type: 'string',
+				alias: 'i',
+			},
+			sound: {
+				type: 'string',
+				alias: 's',
+			},
+		},
 	},
-	flags: {
-		title: {
-			type: 'string',
-			alias: 't'
-		},
-		message: {
-			type: 'string',
-			alias: 'm'
-		},
-		icon: {
-			type: 'string',
-			alias: 'i'
-		},
-		sound: {
-			type: 'string',
-			alias: 's'
-		}
-	}
-});
+);
 
-updateNotifier({pkg}).notify();
+updateNotifier({pkg: package_}).notify();
 
 if (cli.input.length === 0 && cli.flags.v === true) {
 	cli.showVersion();
@@ -66,16 +69,23 @@ if (cli.input.length === 0 && cli.flags.h === true) {
 }
 
 if (cli.input.length !== 1) {
-	console.log(`\n${logSymbols.error} Invalid input. Please check the help below:`);
+	console.log(
+		`\n${logSymbols.error} Invalid input. Please check the help below:`,
+	);
 	cli.showHelp();
 }
 
-if (Object.keys(cli.flags).map(key => typeof cli.flags[key]).some(type => type === 'boolean')) {
-	console.log(`\n${logSymbols.error} Wrong option(s) provided. Please check the help below:`);
+if (
+	Object.keys(cli.flags)
+		.map((key) => typeof cli.flags[key])
+		.includes('boolean')
+) {
+	console.log(
+		`\n${logSymbols.error} Wrong option(s) provided. Please check the help below:`,
+	);
 	cli.showHelp();
 }
 
-errorNotifier(cli.input[0], cli.flags)
-	.catch(error => {
-		process.exit(error.code);
-	});
+errorNotifier(cli.input[0], cli.flags).catch((error) => {
+	process.exit(error.code);
+});
