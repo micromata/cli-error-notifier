5783ac7fa7db809a6a1f0314439687d81dfefccb
Michael Kühnel <m.kuehnel@micromata.de>
2022-09-12 10:29:03 +0200

    chore: Fix ESLint config and errors

--- test/notifier-options.js
+++ test/notifier-options.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import notifierOptions from '../lib/notifier-options';
 
-test('"Default options"', t => {
+test('"Default options"', (t) => {
 	const options = notifierOptions({});
 	t.is(options.title, 'An error has occured');
 	t.is(options.message, 'Check the terminal for more information');
@@ -9,12 +9,12 @@ test('"Default options"', t => {
 	t.true(options.sound);
 });
 
-test('"Flags are returned as notifier options"', t => {
+test('"Flags are returned as notifier options"', (t) => {
 	const options = notifierOptions({
 		title: 'Title',
 		message: 'Message',
 		icon: 'Icon',
-		sound: 'Sound'
+		sound: 'Sound',
 	});
 	t.is(options.title, 'Title');
 	t.is(options.message, 'Message');
@@ -22,12 +22,12 @@ test('"Flags are returned as notifier options"', t => {
 	t.is(options.sound, 'Sound');
 });
 
-test('"Aliases are returned as notifier options"', t => {
+test('"Aliases are returned as notifier options"', (t) => {
 	const options = notifierOptions({
 		t: 'Title',
 		m: 'Message',
 		i: 'Icon',
-		s: 'Sound'
+		s: 'Sound',
 	});
 	t.is(options.title, 'Title');
 	t.is(options.message, 'Message');
@@ -35,12 +35,12 @@ test('"Aliases are returned as notifier options"', t => {
 	t.is(options.sound, 'Sound');
 });
 
-test('"Mute sound with lowercase value"', t => {
+test('"Mute sound with lowercase value"', (t) => {
 	const options = notifierOptions({sound: 'mute'});
 	t.false(options.sound);
 });
 
-test('"Mute sound with uppercase value"', t => {
+test('"Mute sound with uppercase value"', (t) => {
 	const options = notifierOptions({sound: 'Mute'});
 	t.false(options.sound);
 });
